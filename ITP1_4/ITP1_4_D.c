698cfd974075ac6574af8eef24d33f6309b7f8d1
s1260037 <s1260037@u-aizu.ac.jp>
2018-06-21 17:59:04 +0900

    add D

---
+++ ITP1_4/ITP1_4_D.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+  int n;
+  int max = -1000000;
+  int min = 1000000;
+  long long sum = 0;
+  int i;
+  scanf("%d", &n);
+  for(i = 1 ; i <= n ; i++){
+    int a;
+    scanf("%d", &a);
+    sum = sum + a;
+    if(a < min){
+      min = a;
+    }
+    if(a > max){
+      max = a;
+    }
+  }
+  printf("%d %d %lld\n", min, max, sum);
+  return 0;
+}
